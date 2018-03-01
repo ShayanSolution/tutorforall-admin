11a219913699e8127a217087057eac9da5e1c0e2
Arslan Ali <marslan.ali@gmail.com>
2018-03-01 19:08:10 +0500

    production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiURL: 'http://tutor4all-api.shayansolutions.com',
   ApiClientID: '2',
-  apiSec: 'Kav9E0qvHalH5Z8X9yDSy5SDoZDokg6iNS9j567v',
+  apiSec: 'uqEuRQZksrQSAmSwtWj4kUzcjTgCIssvOjUCfKOE',
 };
