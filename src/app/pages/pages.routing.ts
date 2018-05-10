8396135deeaefcf2cc1808caaf1d7951a4c8a250
Dev3 <shayansolutions@gmail.com>
2018-05-10 21:01:34 +0500

    add register route

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -25,12 +25,13 @@ export const routes: Routes = [
       { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
       { path: 'students', loadChildren: './students/students.module#StudentsModule' },
       { path: 'tutors', loadChildren: './tutors/tutors.module#TutorsModule' },
+      { path: 'register', loadChildren: './new/register.module#RegisterModule' },
       { path: 'session/:id', loadChildren: './session/session.module#SessionModule' },
-      //{ path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
-      //{ path: 'components', loadChildren: './components/components.module#ComponentsModule' },
-      //{ path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
-      //{ path: 'ui', loadChildren: './ui/ui.module#UiModule' },
-      //{ path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
+      { path: 'editors', loadChildren: './editors/editors.module#EditorsModule' },
+      { path: 'components', loadChildren: './components/components.module#ComponentsModule' },
+      { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+      { path: 'ui', loadChildren: './ui/ui.module#UiModule' },
+      { path: 'forms', loadChildren: './forms/forms.module#FormsModule' },
       { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
       //{ path: 'maps', loadChildren: './maps/maps.module#MapsModule' }
     ]
