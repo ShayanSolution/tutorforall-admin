544d6386c547ca0bf385611e69d93dc1635c211a
arslan <shayansolutions@gmail.com>
2018-09-14 15:42:12 +0500

    laravel project added

--- src/app/guards/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './AuthenticationGuard';
-export * from './GuestGuard';
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: 'http://tutor4all-api.shayansolutions.com',
-  ApiClientID: '2',
-  apiSec: 'uqEuRQZksrQSAmSwtWj4kUzcjTgCIssvOjUCfKOE',
-};
