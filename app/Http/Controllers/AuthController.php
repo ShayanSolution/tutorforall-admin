2586170736e5b050dcf4da1c5dcf97741199edf2
arslan <shayansolutions@gmail.com>
2018-09-19 16:35:17 +0500

    tutorforall project file added and login admin with server side validations

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('admin.login');
+    }
+    public function authenticate(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            if (Auth::user()->role_id == 1){
+                return redirect()->route('dashboard')->with('success','You are successfully logged in');
+            }else{
+                return redirect()->route('login')->with('error','You are not admin');
+            }
+        }else{
+                return redirect()->route('login')->with('error','Invalid Credentials');
+        }
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login')->with('success','You are logged out successfully');
+    }
+}
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $confirmation_code = str_random(30);
+        $password = Hash::make('admin');
+        DB::table('users')->insert([
+            'uid' => md5(microtime()),
+            'cnic_no' => '123456785451',
+            'fatherName' => 'Quisque',
+            'email' => 'admin@admin.com',
+            'password' => $password,
+            'is_active' => 1,
+            'role_id' => 1,
+            'gender_id' => 1,
+            'experience' => 'N/A',
+            'qualification' => 'N/A',
+            'longitude' => 'N/A',
+            'latitude' => 'N/A',
+            'device_token' => 'N/A',
+            'confirmed' => 0,
+            'username' => 'SuperAdmin',
+            'profileImage' => 'https://png.icons8.com/ios/50/000000/gender-neutral-user.png',
+            'confirmation_code' => $confirmation_code
+        ]);
+    }
+}
