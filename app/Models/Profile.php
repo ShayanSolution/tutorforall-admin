22e3395e7e21f46d176bc56db278e62626bb66dd
arslan <shayansolutions@gmail.com>
2018-10-01 16:08:59 +0500

    added checkbox for deserving student

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $fillable = [
+        'name',
+        'is_mentor',
+        'is_deserving',
+        'meeting_type_id',
+        'user_id',
+        'subject_id',
+        'programme_id',
+        'is_home',
+        'is_group',
+        'one_on_one',
+        'call_tutor',
+        'call_student',
+    ];
     protected $table = 'profiles';
 }
