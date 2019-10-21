05f12ee8ceb8c008ed6d3254fcf477b12c233d51
dev4 <dev1@shayansolutions.com>
2019-10-21 21:19:14 +0500

    changed labels

--- resources/views/admin/candidates.blade.php
+++ resources/views/admin/candidates.blade.php
@@ -5,7 +5,7 @@
         <div class="row bg-title">
             @include('errors.common-errors')
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Candidates List</h4> </div>
+                <h4 class="page-title">Document Verification List</h4> </div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="#">Admin</a></li>
