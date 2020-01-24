8244a728d395bf272f3b0aab08a33ffdc0446d60
Arslan <shayansolutions@gmail.com>
2020-01-24 17:02:12 +0500

    changed page heading

--- resources/views/admin/tutor/map.blade.php
+++ resources/views/admin/tutor/map.blade.php
@@ -1,5 +1,5 @@
 @extends('admin.layout')
-@section('title','Dashboard')
+@section('title','Map')
 
 @section('styles')
     <style>
@@ -19,11 +19,11 @@
     <div class="container-fluid">
         <div class="row bg-title">
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Dashboard</h4> </div>
+                <h4 class="page-title">Map</h4> </div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="#">Admin</a></li>
-                    <li class="active">Dashboard</li>
+                    <li class="active">Map</li>
                 </ol>
             </div>
             <!-- /.col-lg-12 -->
