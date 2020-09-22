bf7a7aae4b964f69060fac6ab251ed5430d180bd
Dev2 <dev2@shayansolutions.com>
2020-09-22 16:31:28 +0500

    Reports Page fast rendering enabled.

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -4,35 +4,52 @@ namespace App\Http\Controllers;
 
 use App\Models\Session;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 
 class ReportsController extends Controller
 {
-    public function tutorReportList(){
-        $tutors = User::select('id', 'firstName', 'lastName')->whereHas('profile', function ($q){
-            $q->where('is_mentor', 0);
-        })->where('role_id',2)->orderBy('id', 'DESC')->get();
-        $tutorList = $tutors->map(function ($tutor,$key) {
-            $tutortemp = collect($tutor);
-            $tutortemp->put('booked',self::getSessionsCount($tutor->id,"booked"));
-            $tutortemp->put('started',self::getSessionsCount($tutor->id,"started"));
-            $tutortemp->put('ended',self::getSessionsCount($tutor->id,"ended"));
-            $tutortemp->put('expired',self::getSessionsCount($tutor->id,"expired"));
-            $tutortemp->put('pending',self::getSessionsCount($tutor->id,"pending"));
-            $tutortemp->put('reject',self::getSessionsCount($tutor->id,"reject"));
-            return $tutortemp;
-        });
-            return view('admin.Reports.reportsList', compact('tutorList'));
+    public function tutorReportList(Request $request)
+    {
+        if ($request->ajax()) {
+            $tutors = User::select('id', 'firstName', 'lastName', 'email', 'phone', 'is_active', 'is_approved', 'created_at', 'last_login')->whereHas('profile', function ($q) {
+                $q->where('is_mentor', 0);
+            })->with('rating')->where('role_id', 2)->orderBy('id', 'DESC');
+            return datatables()->eloquent($tutors)
+                ->addColumn('booked', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "booked");
+                })
+                ->addColumn('started', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "started");
+                })
+                ->addColumn('completed', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "ended");
+                })
+                ->addColumn('missed', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "expired");
+                })
+                ->addColumn('pending', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "pending");
+                })
+                ->addColumn('rejected', function ($tutor) {
+                    return self::getSessionsCount($tutor->id, "reject");
+                })
+                ->rawColumns(['booked', 'started', 'completed', 'missed', 'pending', 'rejected'])
+                ->make();
+        }
+
+        return view('admin.Reports.reportsList');
 
     }
 
-    public static function getSessionsCount($id,$status){
+    public static function getSessionsCount($id, $status)
+    {
         $sessions = Session::whereHas(
             'tutor', function (Builder $query) use ($id) {
             return $query->where('tutor_id', $id);
         }
         )
-            ->where('status',$status)->count();
-        return $sessions!=null?$sessions:0;
+            ->where('status', $status)->count();
+        return $sessions != null ? $sessions : 0;
     }
 }
--- resources/views/admin/Reports/reportsList.blade.php
+++ resources/views/admin/Reports/reportsList.blade.php
@@ -3,14 +3,16 @@
 
 @section('styles')
     <style>
-        .dt-buttons{
+        .dt-buttons {
             margin-top: 10px;
         }
+
         select[name="myTable_length"] {
             padding: 0;
             line-height: 10px;
         }
-        .dataTables_length > label{
+
+        .dataTables_length > label {
             display: inline-block;
         }
     </style>
@@ -21,14 +23,13 @@
         <div class="row bg-title">
             @include('errors.common-errors')
             <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-                <h4 class="page-title">Tutors Session Reports </h4> </div>
+                <h4 class="page-title">Tutors Session Reports </h4></div>
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="#">Admin</a></li>
                     <li class="active">Reports</li>
                 </ol>
             </div>
-            <!-- /.col-lg-12 -->
         </div>
         <div class="row">
             <div class="white-box">
@@ -55,19 +56,6 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($tutorList as $tutor)
-                            <tr>
-                                <td hidden>{{$tutor->get('id')}}</td>
-                                <td>{{$tutor->get('firstName') ? $tutor->get('firstName') : 'N-A'}}</td>
-                                <td>{{$tutor->get('lastName') ? $tutor->get('lastName') : 'N-A'}}</td>
-                                <td>{{$tutor->get('booked')}}</td>
-                                <td>{{$tutor->get('started')}}</td>
-                                <td>{{$tutor->get('ended')}}</td>
-                                <td>{{$tutor->get('expired')}}</td>
-                                <td>{{$tutor->get('pending')}}</td>
-                                <td>{{$tutor->get('reject')}}</td>
-                            </tr>
-                        @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -77,40 +65,75 @@
 @endsection
 @section('javascripts')
     @parent
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
     <script>
 
         $(document).ready(function () {
             let table = $('#myTable').DataTable({
-                dom: '<"row"<"col-sm-8"B><"col-sm-4"fr>><"row"<"col-sm-10"><"col-sm-2"i>>t<"row"<"col-sm-2"l><"col-sm-10"p>>i',
-                buttons: [
-                    { extend: 'csv', className: 'btn-md', exportOptions: {
-                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-                        } },
-                    { extend: 'excel', className: 'btn-md', exportOptions: {
-                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-                        }  },
-                    { extend: 'print', className: 'btn-md', exportOptions: {
-                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
-                        } }
-                ],
-                "language": {
-                    "info": "Total reports : _MAX_ ",
-                }
+                dom: '<"row"<"col-sm-2"l><"col-sm-6"B><"col-sm-4"fr>>t<"row"<"col-sm-2"i><"col-sm-10"p>>',
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ route('reports') }}",
+                    complete: function (data) {
+                        // Switchery
+                        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+                        $('.js-switch').each(function () {
+                            new Switchery($(this)[0], $(this).data());
+                            var base_url = $(this).data('url');
+                        });
 
+                        // Switchery
+                        var elems = Array.prototype.slice.call(document.querySelectorAll('.is_approved_by_admin'));
+                        $('.is_approved_by_admin').each(function () {
+                            new Switchery($(this)[0], $(this).data());
+                            var base_url = $(this).data('url');
+                        });
+                    }
+                },
+                columns: [
+                    {data: 'firstName', name: 'firstName'},
+                    {data: 'lastName', name: 'lastName'},
+                    {data: 'booked', name: 'booked'},
+                    {data: 'started', name: 'started'},
+                    {data: 'completed', name: 'completed'},
+                    {data: 'missed', name: 'missed'},
+                    {data: 'pending', name: 'pending'},
+                    {data: 'rejected', name: 'rejected'},
+                ],
+                buttons: [
+                    {
+                        extend: 'csv', className: 'btn-md', exportOptions: {
+                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+                        }
+                    },
+                    {
+                        extend: 'excel', className: 'btn-md', exportOptions: {
+                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+                        }
+                    },
+                    {
+                        extend: 'print', className: 'btn-md', exportOptions: {
+                            columns: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
+                        }
+                    }
+                ],
+                "bSort": false
             });
 
-            console.log(table.rows().count());
-
             // Event listener to the two range filtering inputs to redraw on input
-            $('#ratings').change(function() {
+            $('#ratings').change(function () {
                 table.draw();
             });
         });
 
+
     </script>
 @stop
 <style>
-    .black-333{
+    .black-333 {
         color: #333333;
     }
 </style>
