611f7fc15edbf2807f831b6bbd3c357077d88dcb
Dev1 <dev1@shayansolutions.com>
2021-02-10 11:39:44 +0500

    fix time zone

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'timezone' => '+05:00',
+            'timezone' => '+00:00',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
